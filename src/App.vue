c434a2b638d5851aaf5cef53c59360351867a3ad
vqlai <vqlai6@163.com>
2026-04-28 21:26:45 +0800

    更新 App.vue，重复添加标题内容以增强可见性

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,10 @@
 
 <template>
   <h1>hello vqlai,You did it!</h1>
+  <h1>hello vqlai,You did it!</h1>
+  <h1>hello vqlai,You did it!</h1>
+  <h1>hello vqlai,You did it!</h1>
+  <h1>hello vqlai,You did it!</h1>
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
