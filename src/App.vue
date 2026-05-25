c6dabc07b2d6f4b71c3868b0d1da6a56ca560195
vqlai <vqlai6@163.com>
2026-05-25 21:44:51 +0800

    优化 App.vue，移除重复标题和多余内容以提升代码整洁性

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,6 @@
 
 <template>
   <h1>hello vqlai,You did it!</h1>
-  <h1>hello vqlai,You did it!</h1>
-  <h1>hello vqlai,You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
 </template>
 
 <style scoped></style>
